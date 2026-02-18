b9444ef5a1f763a490ae2839eff80b87156a74c0
Abhishek Kumar <abhi3144.manu@gmail.com>
2026-02-18 14:14:26 +0530

    added user and admin to play with

--- Backend/User_Services/App.js
+++ Backend/User_Services/App.js
@@ -1,12 +1,15 @@
+const express = require('express')
 const app = require('./Server.js')
+const RegisterRoute = require('./Routers/Register.Router.js');
 const dotenv = require('dotenv')
-
+app.use(express.json())
 dotenv.config()
 
 
 const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT || 4000;
 
 
+app.use(`/api`, RegisterRoute)
 
 
 app.listen(USER_SERVICE_PORT, ()=>{
---
+++ Backend/User_Services/Controllers/RegisterController.js
@@ -0,0 +1,37 @@
+
+const prisma = require("../DB/Prisma.DB");
+const bcrypt = require ('bcrypt')
+
+
+async function RegisterUser(req, res) {
+  const { firstname, lastname, email, password, empId, company } = req.body;
+ 
+  
+ 
+  const hashPass = await bcrypt.hash(password, 10)
+ console.log(hashPass);
+ 
+  
+  try {
+    const user = await prisma.user.create({
+        data : {
+            firstname : firstname,
+            lastname :lastname,
+            email : email,
+            password : hashPass,
+            empId: empId,
+            company: company
+
+        }
+    });
+
+    res.status(201).send("Generated Sucessfully")
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+module.exports = RegisterUser;
